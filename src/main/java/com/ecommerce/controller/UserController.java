f7c1359013b420d46db64db2f29a3ede00988318
Sergio Ramos <sergioramos_92@hotmail.com>
2025-09-01 00:17:22 -0300

    se actualiza controlador de User: crear carrito cuando un usuario se registra

--- src/main/java/com/ecommerce/controller/UserController.java
+++ src/main/java/com/ecommerce/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.dto.UserDTO;
 import com.ecommerce.model.User;
+import com.ecommerce.service.CartService;
 import com.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,15 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final CartService cartService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, CartService cartService) {
         this.userService = userService;
+        this.cartService = cartService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody User user) {
-        User newUser = this.userService.saveUser(user);
-        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    public ResponseEntity<UserDTO> registerUser(@RequestBody User user) {
+        User savedUser = this.userService.saveUser(user);
+        this.cartService.createCartForUser(savedUser);
+        return new ResponseEntity<>(userService.convertToUserDTO(savedUser), HttpStatus.CREATED);
     }
 }
