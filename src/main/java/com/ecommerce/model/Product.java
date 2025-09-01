f64077123de34c7873d59dbbe87ed57854a95c70
Sergio Ramos <sergioramos_92@hotmail.com>
2025-09-01 00:01:09 -0300

    se agrega anotación @Table para entidad Product

--- src/main/java/com/ecommerce/model/Product.java
+++ src/main/java/com/ecommerce/model/Product.java
@@ -1,9 +1,6 @@
 package com.ecommerce.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,7 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
 public class Product {
 
     @Id
