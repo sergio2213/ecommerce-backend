a0de54feb3059baa77da5eed5ec3ef476f2fc250
Sergio Ramos <sergioramos_92@hotmail.com>
2025-10-25 21:35:24 -0300

    se agrega anotación @Repository faltante a CartItemRepository

--- src/main/java/com/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/ecommerce/repository/CartItemRepository.java
@@ -3,9 +3,11 @@ package com.ecommerce.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ecommerce.model.CartItem;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
 }
